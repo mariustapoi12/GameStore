d802b2fa558a2c815b2dccf9ff948aa8640b7da6
Marius-Ștefan Țăpoi <127667249+mariustapoi12@users.noreply.github.com>
2023-11-26 18:48:52 +0200

    Delete frontend directory

--- frontend/src/models/Customer.ts
+++
@@ -1,9 +0,0 @@
-export class Customer{
-    id?: number;
-    firstname: string = "";
-    lastname: string = "";
-    email: string = "";
-    address: string = "";
-    phoneNumber: string = "";
-    totalNumberOfBoughtGames: number = 0;
-}
--- frontend/src/models/Developer.ts
+++
@@ -1,9 +0,0 @@
-export class Developer{
-    id?: number;
-    name: string = "";
-    hq: string = "";
-    publisher: string = "";
-    foundedIn: number = 0;
-    revenue: number = 0;
-    gamesCount: number = 0;
-}
